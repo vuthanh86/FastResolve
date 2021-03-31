a4f97df75bfc1924ba5bced673671e32ad095cc6
Thanh Vu <vuthanh20286@gmail.com>
2021-03-31 23:36:32 +0700

    rebuild from scratch

---
+++ FastDiContainer/FastDiContainer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+
+namespace FastResolve
+{
+    public interface IFastDiContainer
+    {
+        void Register<TSource, TTarget>();
+        void Register(Type sourceType, Type targetType);
+    }
+    public class FastDiContainer : IFastDiContainer
+    {
+        private readonly IDictionary<string, Func<string, Lazy<object>>> _mappingTypes;
+        public FastDiContainer()
+        {
+            _mappingTypes = new ConcurrentDictionary<string, Func<string, Lazy<object>>>();
+        }
+        #region Implementation of IFastDiContainer
+
+        public void Register<TSource, TTarget>()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Register(Type sourceType, Type targetType)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
